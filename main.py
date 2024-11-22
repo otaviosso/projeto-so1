c2ae80f5a3717e4295faf184a7ab3074b84734b2
Otavio <otavioscarparosouza@gmail.com>
2024-11-22 12:08:25 -0300

    Versão final

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ assert A < P, "A não é menor que P"
 
 ships_queue = [queue.Queue() for _ in range(int(S))] # fila de encomendas por ponto de distribuição
 
-remaining_shipments = P
-remaining_shipments_lock = threading.Lock()
+remaining_shipments = P # variavel global que controla quantas encomendas faltam entregar, variavel usada pelo loop dos veiculos
+remaining_shipments_lock = threading.Lock() # lock para modificar corretamente 
 
 pontos_lock = [threading.Lock() for _ in range(int(S))] # uma lock por ponto
-veiculos_queue = [queue.Queue(maxsize=A) for _ in range(int(C))] # fila de encomendas por veículo
+veiculos_queue = [queue.Queue(maxsize=A) for _ in range(int(C))] # fila de encomendas por veículo com tamanho maximo A em cada 
 
 # inicializar arquivo com os logs
 def log_file_initialize(encomenda,event_time):
@@ -32,7 +32,7 @@ def log_file_initialize(encomenda,event_time):
             f.write("\n")
 
 # salvar informações de carregamento/descarregamento de uma encomenda
-def log_shipment_trace(encomenda, status, event_time, vehicle_id=None):
+def log_shipment_trace(encomenda, status, event_time, vehicle_id):
     with open(f"shipment_{encomenda['id']}_trace.txt", "a") as f:
         if status == "loaded":
             f.write(f"Carregada no veiculo {vehicle_id} no horario: {event_time}\n")
@@ -48,20 +48,16 @@ def shipment(i):
     num = i
     
     # gerar ponto de início e de entrega
-    start = random.randint(0, int(S) - 1)
-    
-    dest = random.randint(0, int(S) - 1)
-    while dest == start:
-        dest = random.randint(0, int(S) - 1)
+    start, dest = random.sample(range(0, int(S)), 2)
 
     # Criar e salvar encomenda
     encomenda = {"id": num, "origem": start, "destino": dest, "entregue": False}
-    ships_queue[start].put(encomenda)
+    ships_queue[start].put(encomenda) # Encomenda é colocada na fila do seu ponto de origem
     print(f"Encomenda {num} adicionada ao ponto {start} para o destino {dest}")
 
-    log_file_initialize(encomenda,time.time() - t_0)
+    log_file_initialize(encomenda,time.time() - t_0) # anota as informações iniciais da encomenda
 
-    while not encomenda["entregue"]:
+    while not encomenda["entregue"]: # encomenda fica dormindo enquanto nao for entregue
         time.sleep(1e-6)
     
     print(f"----- encomenda {num} entregue -----")
@@ -75,22 +71,22 @@ def fill_vehicle(start_pos, id):
 
     pos = start_pos
     temp_list = []
-    with pontos_lock[pos]:
-        # Carrega ou descarrega encomendas do veículo
-        while not veiculos_queue[id].empty():
+    with pontos_lock[pos]: # apenas o ponto atual é travado
+        
+        while not veiculos_queue[id].empty():# Verifica se o veiculo tem encomendas para serem entregues, se tiver, verifica cada uma e checa se o destino dela é o ponto atual
             encomenda = veiculos_queue[id].get()
             if encomenda["destino"] == pos:
-                # Log unloading time
+                
                 
                 print(f"Veiculo {id} ENTREGANDO encomenda {encomenda['id']} no ponto {pos}")
                 
                 
-                despacho_tempo = random.uniform(0.5, 2.0)
+                despacho_tempo = random.uniform(0.5, 2.0) # Tempo aleatorio de despacho
                 time.sleep(despacho_tempo)
                 
                 encomenda["entregue"] = True  # Marca como entregue
 
-                log_shipment_trace(encomenda, "unloaded",  time.time() - t_0, id)
+                log_shipment_trace(encomenda, "unloaded",  time.time() - t_0, id) # Anota que a encomenda foi despachada
             else:
                 temp_list.append(encomenda)  # Armazena encomendas que não foram entregues
 
@@ -98,13 +94,13 @@ def fill_vehicle(start_pos, id):
         for encomenda in temp_list:
             veiculos_queue[id].put(encomenda)
         
-        # Carrega novas encomendas para o veículo
+        # Carrega novas encomendas para o veículo se ele tiver espaço
         while not veiculos_queue[id].full() and not ships_queue[pos].empty():
             encomenda = ships_queue[pos].get()
             veiculos_queue[id].put(encomenda)
             print(f"Veiculo {id} pegando encomenda {encomenda['id']} no ponto {pos}")
            
-            log_shipment_trace(encomenda, "loaded",  time.time() - t_0, id)  # tempo de carregamento
+            log_shipment_trace(encomenda, "loaded",  time.time() - t_0, id)  # Anota tempo de carregar
     
 
 def vehicle(i):
@@ -113,14 +109,15 @@ def vehicle(i):
     
     # gerar posição inicial
     start_pos = random.randint(0, int(S) - 1)
-    while remaining_shipments > 0:
+    print(f"Veiculo {num} posicao inicial {start_pos}")
+    while remaining_shipments > 0: #Enquanto houver encomendas a serem entregues, viaja pro proximo ponto
         fill_vehicle(start_pos, num)
-        travel_time = random.uniform(0.5, 2.0)
+        travel_time = random.uniform(0.5, 2.0) # Tempo de viagem aleatorio 
         time.sleep(travel_time)
-        start_pos = (start_pos + 1) % S
+        start_pos = (start_pos + 1) % S # Avança pro proximo ponto na fila circular
 
 # Imprime informações atuais de um ponto
-def print_S(i):
+def points_s(i):
     print(f"S:{i}")
     time.sleep(1)
     while not ships_queue[i].empty():
@@ -132,7 +129,7 @@ def print_S(i):
 # Inicializa e inicia as threads
 threads = []
 for i in range(int(S)):
-    thread = threading.Thread(target=print_S, args=(i,))
+    thread = threading.Thread(target=points_s, args=(i,))
     threads.append(thread)
     thread.start()
 
